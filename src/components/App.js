e406e5cd5141c5dcac9e29782bce2fd94bf895f3
ramiro-e <ramiroestevez96@gmail.com>
2021-09-18 13:01:13 -0300

    Estructura basica del proyecto

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+
+      <Main />
+    </div>
+    
+  );
+}
+
+export default App;

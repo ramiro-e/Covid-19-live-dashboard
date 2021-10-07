4514c3d07c9f6902c36a07228eaf74dd04dcff50
ramiro-e <ramiroestevez96@gmail.com>
2021-10-07 10:50:02 -0300

    Agregando mapa

--- src/components/MapSection.js
+++ src/components/MapSection.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import WorldMap from 'react-svg-worldmap';
 
 
-function MapSection() {
+function MapSection(metrics) {
+  const data = metrics.metrics
+
   return (
     <React.Fragment>
       <div className="col-12 col-md-6">	
-    
+        <div className="card">
+          <WorldMap color="red" valueSuffix="muertes" size="xl" data={data}/>
+        </div> 
       </div>
     </React.Fragment>
 
